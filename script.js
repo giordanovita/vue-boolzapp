f7ac8c7651be6234b902760c8fc3e62ff85fd422
giordanovita <giordanovitag@libero.it>
2021-04-12 11:15:37 +0200

    mileston 1-2

---
+++ script.js
@@ -0,0 +1,214 @@
+function boolZapp(){
+    
+    new Vue({
+        el:'#app',
+        
+        data:{
+            'contacts' : [
+                {
+                name: 'Michele',
+                avatar: "img/tired.jpg",
+                visible: true,
+                messages: [
+                            {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                            },
+                            {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Ricordati di dargli da mangiare',
+                            status: 'sent'
+                            },
+                            {
+                            date: '10/01/2020 16:15:22',
+                            text: 'Tutto fatto!',
+                            status: 'received'
+                            }
+                    ],
+                },
+                {
+                name: 'Fabio',
+                avatar: "img/smile.jpg",
+                visible: true,
+                messages: [
+                            {
+                            date: '20/03/2020 16:30:00',
+                            text: 'Ciao come stai?',
+                            status: 'sent'
+                            },
+                            {
+                            date: '20/03/2020 16:30:55',
+                            text: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                            },
+                            {
+                            date: '20/03/2020 16:35:00',
+                            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                            }
+                    ],
+                },
+                {
+                name: 'Samuele',
+                avatar: "img/trump.png",
+                visible: true,
+                messages: [
+                            {
+                            date: '28/03/2020 10:10:40',
+                            text: 'La Marianna va in campagna',
+                            status: 'received'
+                            },
+                            {
+                            date: '28/03/2020 10:20:10',
+                            text: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                            },
+                            {
+                            date: '28/03/2020 16:15:22',
+                            text: 'Ah scusa!',
+                            status: 'received'
+                            }
+                    ],
+                },
+                {
+                name: 'Luisa',
+                avatar: "img/whatsapp-munch.jpg",
+                visible: true,
+                messages: [
+                            {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                            },
+                            {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                            }
+                            ],
+                            },
+                                
+                    ],
+                    'nameSelected':'',
+                    'avatarSelected':'',
+                    'selectedChat':0,
+                    'searchContact':'',
+                    'myMsgs':'',
+                    'sendMessages':[],
+                    'msg': '',
+                    'filtered':[],
+                    'search':'',
+                    'finded':''
+                    
+                },
+
+               
+            methods:{
+
+               
+
+              
+                 chooseChat:function(index){
+                    this.selectedChat=index;
+                    console.log(index)
+                     this.nameSelected  = this.contacts[index].name
+                     this.avatarSelected = this.contacts[index].avatar
+
+                     const contact = this.contacts[index];
+                     const contactMsgs = contact['messages']
+                     let contactMsgsValue = contactMsgs.status;
+
+                    for(i=0; i<contactMsgs.length; i++){
+                        const contactMess = contactMsgs[i];
+
+
+                    } 
+      //--------     
+                    
+                  
+                 }, 
+                  autoAnswer: function (index) {
+                   
+                    let moment = new Date();
+                    let date = `${moment.getDate()}/${('0'+moment.getMonth() + 1)}/${moment.getFullYear().toString().slice(2)}`;  
+                    let minutes = moment.getMinutes();
+                        if(minutes.length<2){
+                            return minutes= minutes+'0'
+                        }
+                        console.log(minutes)
+                    let hours = `${moment.getHours()}:${minutes}`;
+    
+                    let textArr=[ 'ok!' , 'ho capito', 'va bene!', 'perchè?', 'hahahaha', 'non ho capito'];
+                    let getRandomAnswer = Math.floor(Math.random()*(textArr.length));
+                    let text = textArr[getRandomAnswer];
+
+                    let status = 'received';
+                    let contactControl = this.contacts[index].messages.length;
+                    const intervalTool = this.contacts[index].messages;
+
+                    if(contactControl++){
+                        setTimeout(function(index){
+                            intervalTool.push({date, hours,text, status});
+                            console.log( getRandomAnswer,text)
+                         },3000) 
+                    }
+                }, 
+
+                enterMsgs: function (index) {
+                    let moment = new Date();
+                    let date = `${moment.getDate()}/${'0'+(moment.getMonth() + 1)}/${moment.getFullYear().toString().slice(2)}`;  
+                    let minutes = moment.getMinutes();
+                        if(minutes.length<2){
+                            return minutes= minutes+'0'
+                        }
+                        console.log(minutes)
+                    let hours = `${moment.getHours()}:${minutes}`;
+    
+                    let text = this.myMsgs;
+                    let status = 'sent';
+    
+                    if(this.myMsgs.length > 0){
+                        this.contacts[index].messages.push({date, hours, text, status});
+                        this.myMsgs = '';
+                    }
+                let getAnswer =  this.autoAnswer(index);
+                  return  getAnswer; 
+                  
+                }
+            ,
+
+                
+
+              
+                 filteredContacts(key) {
+                  //  function filter(array, key) {
+                      for(let i = 0; i<this.contacts.length;i++){
+                         //this.filtered = this.contacts[i].name;
+                        // this.finded= this.filtered[i]
+                         if(key.includes(this.contacts[i].name)){
+                             this.filtered.push(this.key)
+                             return this.filtered
+                         }
+
+                          console.log(this.filtered)
+                         console.log(key)
+                         // console.log(this.finded)
+
+
+                        }
+
+                           /*  if(key.includes(this.contacts[i].name)) {
+                                filtered.push(this.contacts[i].name)
+                            } */
+                      }
+                    }
+                    
+        })
+    }
+
+
+function init(){
+    boolZapp()
+}
+document.addEventListener('DOMContentLoaded', init);
