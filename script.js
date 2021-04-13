9851d95436f20f67c15d53c66ce14b72a7d56d80
giordanovita <giordanovitag@libero.it>
2021-04-13 17:42:33 +0200

    prova bonus

--- script.js
+++ script.js
@@ -8,83 +8,91 @@ function boolZapp(){
                 {
                 name: 'Michele',
                 avatar: "img/tired.jpg",
-                visible: true,
                 messages: [
                             {
                             date: '10/01/2020 15:30:55',
                             text: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            status: 'sent',
+                            visible: false,
                             },
                             {
                             date: '10/01/2020 15:50:00',
                             text: 'Ricordati di dargli da mangiare',
-                            status: 'sent'
+                            status: 'sent',
+                            visible: false,
                             },
                             {
                             date: '10/01/2020 16:15:22',
                             text: 'Tutto fatto!',
-                            status: 'received'
+                            status: 'received',
+                            visible: false,
                             }
                     ],
                 },
                 {
                 name: 'Fabio',
                 avatar: "img/smile.jpg",
-                visible: true,
                 messages: [
                             {
                             date: '20/03/2020 16:30:00',
                             text: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            visible: false,
                             },
                             {
                             date: '20/03/2020 16:30:55',
                             text: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            status: 'received',
+                            visible: false
                             },
                             {
                             date: '20/03/2020 16:35:00',
                             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            visible: false
                             }
                     ],
                 },
                 {
                 name: 'Samuele',
                 avatar: "img/trump.png",
-                visible: true,
+                
                 messages: [
                             {
                             date: '28/03/2020 10:10:40',
                             text: 'La Marianna va in campagna',
-                            status: 'received'
+                            status: 'received',
+                            visible: false,
                             },
                             {
                             date: '28/03/2020 10:20:10',
                             text: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            visible: false,
                             },
                             {
                             date: '28/03/2020 16:15:22',
                             text: 'Ah scusa!',
-                            status: 'received'
+                            status: 'received',
+                            visible: false,
                             }
                     ],
                 },
                 {
                 name: 'Luisa',
                 avatar: "img/whatsapp-munch.jpg",
-                visible: true,
                 messages: [
                             {
                             date: '10/01/2020 15:30:55',
                             text: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            visible: false,
                             },
                             {
                             date: '10/01/2020 15:50:00',
                             text: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            status: 'received',
+                            visible: false,
                             }
                             ],
                             },
@@ -94,11 +102,17 @@ function boolZapp(){
                     'avatarSelected':'',
                     'selectedChat':0,
                     'myMsgs':'',
-                    'sendMessages':[],
                     'search':'',
                     'convcontent':false,
-                    'intro':true
+                    'intro':true,
+                    'popdel':false,
+                    'msgSelected':0
                     
+                },
+
+                updated(){
+                    var container = this.$el.querySelector(".conv");
+                     container.scrollTop = container.scrollHeight;
                 },
      
                 computed:{
@@ -111,36 +125,27 @@ function boolZapp(){
                         });
                         return filtered;
                     },  
+
+                   
                 },
                
             methods:{
 
                  chooseChat:function(el){
-                     const index= this.contacts.indexOf(el);
                      this.convcontent=true;
                      this.intro=false;
+                     const index= this.contacts.indexOf(el);
                      this.selectedChat=index;
                      this.nameSelected  = this.contacts[index].name;
                      this.avatarSelected = this.contacts[index].avatar;
-
-                     const contact = this.contacts[index];
-                    const contactMsgs = contact['messages']
-                     let contactMsgsValue = contactMsgs.status;
- 
-                    for(i=0; i<contactMsgs.length; i++){
-                        const contactMess = contactMsgs[i];
-                    } 
+                    
                   
                  }, 
                   autoAnswer: function (index) {
                    
                     let moment = new Date();
                     let date = `${moment.getDate()}/${('0'+moment.getMonth() + 1)}/${moment.getFullYear().toString().slice(2)}`;  
-                    let minutes = moment.getMinutes();
-                        if(minutes.length<2){
-                            return minutes='0'+ minutes;
-                        } 
-                        console.log(minutes)
+                    let minutes = this.formatminuts(moment.getMinutes());
                     let hours = `${moment.getHours()}:${minutes}`;
     
                     let textArr=[ 'ok!' , 'ho capito', 'va bene!', 'perchè?', 'hahahaha', 'non ho capito'];
@@ -148,42 +153,72 @@ function boolZapp(){
                     let text = textArr[getRandomAnswer];
 
                     let status = 'received';
+                    let visible = true;
+
                     let contactControl = this.contacts[index].messages.length;
                     const intervalTool = this.contacts[index].messages;
 
-                    if(contactControl++){
-                        setTimeout(function(index){
-                            intervalTool.push({date, hours,text, status});
-                            console.log( getRandomAnswer,text)
-                         },1500) 
-                    }
+                        if(contactControl++){
+                            setTimeout(function(){
+                                intervalTool.push({date, hours,text, status, visible});
+                                console.log( getRandomAnswer,text)
+                            },1500) 
+                        }
                     
                 }, 
 
                 enterMsgs: function (index) {
                     let moment = new Date();
                     let date = `${moment.getDate()}/${'0'+(moment.getMonth() + 1)}/${moment.getFullYear().toString().slice(2)}`;  
-                    let minutes =  moment.getMinutes();
+                    let minutes =  this.formatminuts(moment.getMinutes());;
                     let hours = `${moment.getHours()}:${minutes}`;
-                     if(minutes<10){
-                         minutes = '0'+ minutes;
-                     }
-
-    
                     let text = this.myMsgs;
                     let status = 'sent';
+                    let visible = true;
     
                         if(this.myMsgs.length > 0){
-                            this.contacts[index].messages.push({date, hours, text, status});
+                            this.contacts[index].messages.push({date, hours, text, status, visible});
                             this.myMsgs = '';
                         }
                     let getAnswer =  this.autoAnswer(index);
-                    
                     return  getAnswer; 
                   
                         },
 
+                    formatminuts:function(i){
+                        if(i<10){
+                            i='0'+i;
+                        }
+                         return i;
+                    },
+
+                   /*  showPop:function(index){
+                        this.popdel=true
+
+                    },
+
+                    hidePop:function(index){
+                            this.popdel=false
+                    }, */
+
+                    showPopDel:function(){
+                       // this.popdel=true
+
+                        //const index= this.contacts.indexOf(el);
+                       // const index= this.contacts[this.selectedChat].messages; //accedo ai messaggi della chat selezionata
+                        //let superIndex= index[this.msgSelected].visible//accedo allo al visible del singolo mess 
+                        //console.log(superIndex)
+                        if(this.contacts[this.selectedChat].messages[this.msgSelected].visible=true){
+                            
+                        }else{
+                            return this.contacts[this.selectedChat].messages[this.msgSelected].visible=true, this.popdel=true
+
+                        }
+                       
                         
+                    }, 
+
+                  
                     }
                                 
         })
@@ -194,3 +229,6 @@ function init(){
     boolZapp()
 }
 document.addEventListener('DOMContentLoaded', init);
+
+
+
